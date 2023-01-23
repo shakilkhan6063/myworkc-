98e5d17df1c7881fbdf626c51b47d6ef3f4650ba
shakil khan <122600220+shakilkhan6063@users.noreply.github.com>
2023-01-24 00:36:00 +0600

    Even_number

---
+++ even.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a;
+
+cout<<" enter then number of a : ";
+cin>>a;
+
+if(a%2==0)
+{
+    cout<<"even number";
+}
+else
+    cout<<"odd number";
+return 0;
+
+
+}
+
+
