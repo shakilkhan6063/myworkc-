2d4d8e4a0a2289210649f84fed8d5d4441b41032
shakil khan <122600220+shakilkhan6063@users.noreply.github.com>
2023-01-24 23:10:22 +0600

    Add files via upload

---
+++ labsalary.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class {
+    int i;
+
+public:
+    samp(int n){
+    i=n;
+    }
+    int get_i(){
+    return i;
+    }
+
+
+};
+int square(smap obj){
+return obj.get_i()*obj.get_i()
+}
+int main(){
+sam ob1(5),ob2(55);
+cout<<"squre of object"<<square(ob1)<<endl;
+cout<<"squre of object"<<square(ob1)<<endl;
+
+return 0;
+
+
+}
